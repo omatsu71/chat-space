e6ef5dfa45e3ad8fb20e1e5232a6b11794954be5
omatsu71 <jampamam1223@gmail.com>
2019-12-03 05:21:58 +0900

    コントローラー

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,14 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
   
 end
