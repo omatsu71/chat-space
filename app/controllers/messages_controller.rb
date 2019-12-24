76b731bb8b2be0b095e843a87b102f58fea20dc9
omatsu71 <jampamam1223@gmail.com>
2019-12-24 16:13:46 +0900

    API

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,21 +4,15 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    # respond_to do |format|
-    #   format.html
-    #   format.json{ @new_messages = @messages.where('id > ?', params[:id]) }
-    # end
   end
 
   
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      # respond_to do |format|
-      #   format.html {redirect_to group_messages_path(@group)}
-      #   format.json
-      # end
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
