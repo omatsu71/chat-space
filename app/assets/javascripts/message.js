330e77c77d7fc39d78b62f3c958592a86a78cbdb
omatsu71 <jampamam1223@gmail.com>
2020-01-04 08:05:42 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
     if (message.image) {
       var html = `<div class="message" data-message-id= ${message.id}>
                     <div class="upper-message">
-                      <div class="pper-message__user-name">
+                      <div class="upper-message__user-name">
                         ${message.user_name}
                       </div>
                       <div class="upper-message__date">
@@ -28,7 +28,7 @@ $(function(){
                       </div>
                     </div>
                     <div class="lower-message">
-                      <p class="lower-message__image">
+                      <p class="lower-message__content">
                         ${message.content}
                       </p>
                   </div>
@@ -48,8 +48,9 @@ $(function(){
       processData: false,
       contentType: false
     })
-     .done(function(data){
+    .done(function(data){
       var html = buildHTML(data);
+      $("#new_message")[0].reset();
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
